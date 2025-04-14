b2921ea61dde052cb19bf90bfca7b8ff9054637c
chenjm34827 <chenjm34827@hundsun.com>
2025-04-14 16:47:58 +0800

    优化代码

--- ReturnClassification/train_and_test.py
+++ ReturnClassification/train_and_test.py
@@ -45,7 +45,6 @@ def split_train_test_data(fund_code, future_days, df_price, metrics_df,
     # 合并指标数据和价格数据，得到包含特征和标签的数据集
     df_all = pd.merge(metrics_df, df_price_renamed[["date", f"label_up_{future_days}d"]],
                       how="inner", on="date")
-    print(df_all.head())
 
     ''' 划分数据集 '''
     # 定义训练集和测试集的时间区间
@@ -53,14 +52,13 @@ def split_train_test_data(fund_code, future_days, df_price, metrics_df,
     train_end = pd.to_datetime(train_end)
     test_start = pd.to_datetime(test_start)
     test_end = pd.to_datetime(test_end)
-    print(f"训练集时间区间: {train_start} ~ {train_end}")
-    print(f"测试集时间区间: {test_start} ~ {test_end}")
+    print(f"训练集时间区间: {train_start} ~ {train_end}; 测试集时间区间: {test_start} ~ {test_end}")
 
     # 根据时间区间划分训练集和测试集
     train_df = df_all[(df_all['date'] >= train_start) & (df_all['date'] <= train_end)]
     test_df = df_all[(df_all['date'] >= test_start) & (df_all['date'] <= test_end)]
-    print(f"训练集数据量: {len(train_df)}, 共 {len(train_df.columns)} 列")
-    print(f"测试集数据量: {len(test_df)}, 共 {len(test_df.columns)} 列")
+    print(f"训练集数据量: {len(train_df)}, 共 {len(train_df.columns)} 列; "
+          f"测试集数据量: {len(test_df)}, 共 {len(test_df.columns)} 列")
 
     ''' 准备特征和标签 '''
     # 选取特征列和目标列
@@ -76,8 +74,57 @@ def split_train_test_data(fund_code, future_days, df_price, metrics_df,
     return x_train, y_train, x_test, y_test
 
 
+def main_data_prepare(the_fund_code='159919.SZ',
+                      n_days=20,
+                      folder_path='../Data',
+                      metrics_folder='../Data/Metrics',
+                      train_start=None,
+                      train_end='2023-12-31',
+                      test_start='2024-01-01',
+                      test_end='2025-03-31'
+                      ):
+    """
+    主要数据准备函数，用于准备基金数据以进行后续的机器学习模型训练和测试。
+
+    :param the_fund_code: 基金代码，默认为'159919.SZ'
+    :param n_days: 用于计算未来收益的天数，默认为20天
+    :param folder_path: 基金价格数据的文件夹路径，默认为'../Data'
+    :param metrics_folder: 基金指标数据的文件夹路径，默认为'../Data/Metrics'
+    :param train_start: 训练集开始日期，如果为None，则从数据的开始日期开始
+    :param train_end: 训练集结束日期，默认为'2023-12-31'
+    :param test_start: 测试集开始日期，默认为'2024-01-01'
+    :param test_end: 测试集结束日期，默认为'2025-03-31'
+    :return: 返回训练集特征、训练集标签、测试集特征、测试集标签和原始指标数据
+    """
+    ''' 价格数据预处理 '''
+    # 获取收盘价数据
+    close_price = get_fund_close_price(the_fund_code, folder_path)
+    # 滚动计算未来5天的对数收益率
+    close_price = cal_future_log_return(close_price, n_days=n_days)
+    # 生成目标标签: 未来收益大于0的样本标记为1，否则为0
+    close_price[f"label_up_{n_days}d"] = (close_price[f"log_return_forward_{n_days}d"] > 0).astype(int)
+
+    ''' 指标数据预处理 '''
+    # 获取指标数据
+    metrics_data = get_fund_metrics_data(the_fund_code, metrics_folder)
+    # 预处理指标数据
+    metrics_data = preprocess_data(metrics_data)
+
+    ''' 测试集训练集划分 '''
+    # 划分训练集和测试集数据
+    x_train, y_train, x_test, y_test = split_train_test_data(
+        the_fund_code, n_days, close_price, metrics_data,
+        train_start=train_start,
+        train_end=train_end,
+        test_start=test_start,
+        test_end=test_end
+    )
+    # 返回划分好的数据集和原始指标数据
+    return x_train, y_train, x_test, y_test, metrics_data
+
+
 # 使用随机森林分类器进行训练和测试，并进行超参数优化和特征重要性评估
-def train_and_test_random_forest(x_train, x_test, y_train, y_test, feature_cols,
+def train_and_test_random_forest(x_train, x_test, y_train, y_test, metrics_data,
                                  random_seed=42, n_iter=20, cv=5):
     """
     使用随机森林分类器进行训练和测试，并进行超参数优化和特征重要性评估。
@@ -87,7 +134,7 @@ def train_and_test_random_forest(x_train, x_test, y_train, y_test, feature_cols,
     :param x_test: 测试集特征数据
     :param y_train: 训练集标签
     :param y_test: 测试集标签
-    :param feature_cols: 特征列名称列表
+    :param metrics_data: 指标结果dataframe
     :param random_seed: 随机种子，用于确保结果可重复
     :param n_iter: 随机搜索的迭代次数
     :param cv: 交叉验证的折数
@@ -97,6 +144,10 @@ def train_and_test_random_forest(x_train, x_test, y_train, y_test, feature_cols,
     from sklearn.metrics import classification_report
     from sklearn.model_selection import RandomizedSearchCV
     from scipy.stats import randint
+
+    # 筛选特征列
+    feature_cols = [
+        col for col in metrics_data.columns if col not in ['ts_code', 'date']]
 
     ''' 自动超参数调优 '''
     # 定义随机搜索的超参数空间
@@ -183,50 +234,50 @@ def train_and_test_random_forest(x_train, x_test, y_train, y_test, feature_cols,
 
 
 # 未来收益分类主函数，用于执行基金数据预处理、模型训练等任务 (使用随机森林)
-def predict_main_random_forest(the_fund_code='159919.SZ', n_days=20,
-                               folder_path='../Data', metrics_folder='../Data/Metrics'
+def predict_main_random_forest(the_fund_code='159919.SZ',
+                               n_days=20,
+                               folder_path='../Data',
+                               metrics_folder='../Data/Metrics',
+                               train_start=None,
+                               train_end='2023-12-31',
+                               test_start='2024-01-01',
+                               test_end='2025-03-31',
+                               random_seed=42, n_iter=20, cv=5
                                ):
     """
-    主函数，用于执行基金数据预处理、模型训练等任务。
+    使用随机森林模型预测基金走势。
 
-    :param the_fund_code: 基金代码，默认为'159919.SZ'
-    :param n_days: 用于计算未来对数收益率的天数，默认为20天
-    :param folder_path: 数据文件夹路径，默认为'../Data'
-    :param metrics_folder: 指标数据文件夹路径，默认为'../Data/Metrics'
-    :return: 训练完成的模型
+    :param the_fund_code: 基金代码，默认为 '159919.SZ'。
+    :param n_days: 用于预测的天数，默认为 20 天。
+    :param folder_path: 数据文件夹路径，默认为 '../Data'。
+    :param metrics_folder: 评价指标文件夹路径，默认为 '../Data/Metrics'。
+    :param train_start: 训练数据开始日期，默认为 None。
+    :param train_end: 训练数据结束日期，默认为 '2023-12-31'。
+    :param test_start: 测试数据开始日期，默认为 '2024-01-01'。
+    :param test_end: 测试数据结束日期，默认为 '2025-03-31'。
+    :param random_seed: 随机种子，默认为 42。
+    :param n_iter: 随机搜索的迭代次数，默认为 20 次。
+    :param cv: 交叉验证的折数，默认为 5 折。
+    :return: 训练完成的随机森林模型。
     """
-    ''' 价格数据预处理 '''
-    # 获取收盘价数据
-    close_price = get_fund_close_price(the_fund_code, folder_path)
-    # 滚动计算未来5天的对数收益率
-    close_price = cal_future_log_return(close_price, n_days=n_days)
-    # 生成目标标签
-    close_price[f"label_up_{n_days}d"] = (close_price[f"log_return_forward_{n_days}d"] > 0).astype(int)
-
-    ''' 指标数据预处理 '''
-    # 获取指标数据
-    metrics_data = get_fund_metrics_data(the_fund_code, metrics_folder)
-    # 预处理指标数据
-    metrics_data = preprocess_data(metrics_data)
-
-    ''' 测试集训练集划分 '''
-    # 划分训练集和测试集数据
-    x_train, y_train, x_test, y_test = split_train_test_data(
-        the_fund_code, n_days, close_price, metrics_data,
-        train_start=None,
-        train_end='2023-12-31',
-        test_start='2024-01-01',
-        test_end='2025-03-31'
+    ''' 数据准备 '''
+    # 准备数据，包括训练集和测试集，以及评价指标数据
+    x_train, y_train, x_test, y_test, metrics_data = main_data_prepare(
+        the_fund_code=the_fund_code,
+        n_days=n_days,
+        folder_path=folder_path,
+        metrics_folder=metrics_folder,
+        train_start=train_start,
+        train_end=train_end,
+        test_start=test_start,
+        test_end=test_end
     )
 
     ''' 训练模型 '''
-    # 筛选特征列
-    feature_cols = [
-        col for col in metrics_data.columns if col not in ['ts_code', 'date']]
     # 训练随机森林模型
     trained_model = train_and_test_random_forest(
-        x_train, x_test, y_train, y_test, feature_cols,
-        random_seed=42, n_iter=20, cv=5)
+        x_train, x_test, y_train, y_test, metrics_data,
+        random_seed=random_seed, n_iter=n_iter, cv=cv)
     # 输出模型训练完成的信息
     print("模型训练完成")
     # 返回训练完成的模型
@@ -234,7 +285,7 @@ def predict_main_random_forest(the_fund_code='159919.SZ', n_days=20,
 
 
 if __name__ == '__main__':
-    predict_main_random_forest(the_fund_code='159919.SZ', n_days=25,
-                               folder_path='../Data', metrics_folder='../Data/Metrics',
-                               )
-    
+    for d in [3, 5, 10, 15, 20, 30]:
+        predict_main_random_forest(the_fund_code='159919.SZ', n_days=d,
+                                   folder_path='../Data', metrics_folder='../Data/Metrics',
+                                   )
