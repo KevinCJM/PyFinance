d071c969faffb38ab830ac160586fab1a5b5c07b
chenjm34827 <chenjm34827@hundsun.com>
2025-07-10 12:18:46 +0800

    111

--- AutoFactorCreator/A04_AgentTools.py
+++ AutoFactorCreator/A04_AgentTools.py
@@ -3,5 +3,5 @@
 @File: A04_AgentTools.py
 @Modify Time: 2025/7/10 10:40       
 @Author: Kevin-Chen
-@Descriptions: 
+@Descriptions: 提供给智能体使用的工具,包括: ReadFile, SearchText, FindFile, EditFile, CreateFile, Shell, 等等
 """
