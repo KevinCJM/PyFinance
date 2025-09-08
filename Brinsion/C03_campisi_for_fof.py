a91ac8b5fcdf220b3f673b6d0ec292c0dca32604
chenjm34827 <chenjm34827@hundsun.com>
2025-09-08 15:36:22 +0800

    rename

--- Brinsion/C03_campisi_for_fof.py
+++ Brinsion/C03_campisi_for_fof.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 """
-@File: C03_campisi_for_fund.py
+@File: C02_campisi_for_fund.py
 @Modify Time: 2025/9/8 15:29       
 @Author: Kevin-Chen
 @Descriptions: Campisi归因, 计算FOF组合的因子
