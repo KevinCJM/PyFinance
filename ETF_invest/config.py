0919ae4976c10c9cd6922cb8fb6031abfb6d8289
chenjm34827 <chenjm34827@hundsun.com>
2025-09-22 20:08:58 +0800

    将有效前沿API嵌入到总文件夹中

---
+++ ETF_invest/config.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+"""
+@File: config.py.py
+@Modify Time: 2025/9/22 19:34       
+@Author: Kevin-Chen
+@Descriptions: 各类配置参数
+"""
+
+TUSHARE_TOKEN = "YOUR_TOKEN"  # TODO: 在此填写你的 Tushare Pro 令牌
