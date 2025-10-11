cb983533433beb6366d9499f3533f967d4fa20f0
chenjm34827 <chenjm34827@hundsun.com>
2025-10-11 10:37:41 +0800

    删除 ETF_invest 相关文件和文件夹

--- ETF_invest/config.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""
-@File: config.py
-@Modify Time: 2025/9/22 19:34       
-@Author: Kevin-Chen
-@Descriptions: 各类配置参数
-"""
-
-TUSHARE_TOKEN = "cdcff0dd57ef63b6e9a347481996ea8f555b0aae35088c9b921a06c9"  # TODO: 在此填写你的 Tushare Pro 令牌
